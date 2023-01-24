59a3ed117d89cc64c767effb9094b2d0e9d8daf7
Abdullah-Ashehry <abboodfs@gmail.com>
2023-01-24 23:22:41 +0300

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -88,9 +88,11 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
+        print(vote1)
         # TODO: use tracer object to trace cat vote
         tracer.span(name=vote1)
         vote2 = r.get(button2).decode('utf-8')
+        print(vote2)
         # TODO: use tracer object to trace dog vote
         tracer.span(name=vote2)
 
@@ -109,10 +111,13 @@ def index():
             # TODO: use logger object to log cat vote
 
             logger.info('Cats Vote: %s', vote1, extra=properties)
+            print('Cats Vote: %s', vote1, extra=properties)
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
+
             logger.info('Dogs Vote: %s', vote2, extra=properties)
+            print('Dogs Vote: %s', vote2, extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
